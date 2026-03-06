3193a1cf170490cbab479b64305f2b1e51fd388a
darlan <darlansalinodias52@gmail.com>
2026-03-06 08:50:43 -0400

    tela entrar ou fazer login adicionado e botao cadastrar funcional

--- app/src/main/java/Fragments/Fragment_entrar.java
+++ app/src/main/java/Fragments/Fragment_entrar.java
@@ -1,6 +1,5 @@
 package Fragments;
 
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,19 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.appnoticia.R;
-public class Fragment_tela_inicial extends Fragment {
 
-    Button btn_cadastrar;
-
+public class Fragment_entrar extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment__tela__inicial, container, false);
+        View view = inflater.inflate(R.layout.fragment_entrar, container, false);
+
 
 
         return view;
--- app/src/main/java/com/example/appnoticia/MainActivity.java
+++ app/src/main/java/com/example/appnoticia/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.appnoticia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentTransaction;
-
-import Fragments.Fragment_tela_inicial;
-import Fragments.Fragment_cadastro;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_cadastrar,btn_entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +26,24 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.frame_principal,new Fragment_tela_inicial());
-        transaction.commit();
+        btn_cadastrar = findViewById(R.id.btn_casdastrar);
+        btn_entrar = findViewById(R.id.btn_entrar);
+
+
+        btn_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), segunda_activity.class);
+                intent.putExtra("SHOW_FRAGMENT",0);
+                startActivity(intent);
+            }
+        });
+
+        
+
+
+        //FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //transaction.add(R.id.frame_principal,new Fragment_tela_inicial());
+        //transaction.commit();
     }
 }
